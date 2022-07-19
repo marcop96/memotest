230a5e93d18f72e9d8839a725be6dd2245713302
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-19 07:30:19 +0200

    check if tiles are the sameWIP

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ let $SQUARES = $BOARD.querySelectorAll("#square");
 let $END_GAME_DIV = document.querySelector("end-game");
 let $firstSquare = null;
 let currentTile;
-
+let turns = 0;
 function configGame() {
   const colors = ["blue", "red", "green", "yellow", "pink", "orange"];
   const duplicateColors = colors.concat(colors);
@@ -21,11 +21,23 @@ function randomizeColors($SQUARES, colors) {
     $SQUARES[i].classList.add(color);
   });
 }
+function tileHandler($currentTile) {
+  showTiles($currentTile);
+  //if its null its the first tile clicked
+  if ($firstSquare === null) {
+    $firstSquare = $currentTile; // so it sets it up as current tile
+  } else {
+    if ($firstSquare === $currentTile) {
+      return;
+    }
+  }
+}
 
 $BOARD.onclick = function (e) {
   const $currentTile = e.target;
   showTiles($currentTile);
   tileHandler($currentTile);
+  correctTile($firstSquare, $currentTile);
 };
 
 function hideTiles() {
@@ -36,11 +48,11 @@ function hideTiles() {
 function showTiles(tile) {
   tile.classList.remove("occult");
 }
-function correctTile() {}
-configGame();
-
-function tileHandler($currentTile) {
-  if ($firstSquare === null) {
-    $firstSquare = $currentTile;
+function correctTile($firstSquare, $currentTile) {
+  if ($firstSquare.className === $currentTile.className) {
+    console.log("iguales");
+  } else {
+    console.log("no");
   }
 }
+configGame();
