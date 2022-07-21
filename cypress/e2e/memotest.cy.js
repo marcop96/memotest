2ab2d0cdf5c35d52c2076c50f32e7c03236c49ba
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-21 20:20:55 +0200

    create first test
    tests  that the site works and if theres a board and 12 tiles

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,5 +1,11 @@
+const URL = "http://127.0.0.1:5500/src/";
+const TILE_QTY = 12;
 describe("Se asegura que funcione el sitio", () => {
   it("passes", () => {
-    cy.visit("http://127.0.0.1:5500/src/");
+    cy.visit(URL);
+    cy.get("#board").find(".square").should("have.length", TILE_QTY);
   });
 });
+
+// it("assures that theres a board and tiles", () => {
+// });
