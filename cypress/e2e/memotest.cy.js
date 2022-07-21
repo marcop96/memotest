d93ba9113cfd5a5050d3ac949119081a3e13332b
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-21 20:38:31 +0200

    deletes unnecesary file

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,11 +1,12 @@
 const URL = "http://127.0.0.1:5500/src/";
 const TILE_QTY = 12;
-describe("Se asegura que funcione el sitio", () => {
-  it("passes", () => {
+
+context("memotest", () => {
+  before(() => {
     cy.visit(URL);
-    cy.get("#board").find(".square").should("have.length", TILE_QTY);
   });
 });
 
-// it("assures that theres a board and tiles", () => {
-// });
+it("checks if theres a board and tiles", () => {
+  cy.get("#board").find(".square").should("have.length", TILE_QTY);
+});
