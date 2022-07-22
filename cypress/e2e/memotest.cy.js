b4737da3047e88f68bccc89959406a3716901112
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-22 18:08:35 +0200

    fully tests memotest!

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,13 +1,90 @@
 const URL = "http://127.0.0.1:5500/src/";
-const TILE_QTY = 12;
 
-context("memotest", () => {
+context("Memotest", () => {
   before(() => {
     cy.visit(URL);
   });
-  it("checks if theres a board and tiles", () => {
-    cy.get("#board").find(".square").should("have.length", TILE_QTY);
+
+  describe("plays memotest", () => {
+    const TILE_QTY = 12;
+
+    it("checks if theres a board and tiles", () => {
+      cy.get("#board").find(".square").should("have.length", TILE_QTY);
+    });
+
+    it("checks that tiles are random", () => {
+      cy.get(".square").then((tiles) => {
+        let originalClasses = [];
+        tiles.each(function (i, tile) {
+          originalClasses.push(tile.className);
+        });
+
+        cy.visit(URL);
+
+        let newClasses = [];
+        cy.get(".square").then((newTiles) => {
+          newTiles.each(function (i, tile) {
+            newClasses.push(tile.className);
+          });
+
+          cy.wrap(originalClasses).should("not.deep.equal", newClasses);
+        });
+      });
+    });
+
+    describe("solves the game", () => {
+      let pairsMap, pairsList;
+      it("chooses a wrong combination", () => {
+        cy.get(".square").then((tiles) => {
+          pairsMap = getTilePairs(tiles);
+          pairsList = Object.values(pairsMap);
+
+          // console.log(pairsList);
+          cy.get(pairsList[0][0]).click();
+          cy.get(pairsList[1][0]).click();
+
+          cy.get(".square").should("have.length", TILE_QTY);
+        });
+      });
+
+      it("resuelve el juego", () => {
+        cy.get(".square").should("have.length", TILE_QTY);
+
+        pairsList.forEach((pair) => {
+          cy.get(pair[0]).click();
+          cy.get(pair[1]).click();
+        });
+
+        cy.get(".cuadro").should("have.length", 0);
+
+        cy.get("#board").should("not.be.visible");
+        const turns = TILE_QTY / 2 + 1; //there was an incorrect move
+        cy.get(".end-game")
+          .should("be.visible")
+          .contains(`congratulations! you won in ${turns} turns`);
+      });
+    });
   });
 });
 
-//doesnt work
+function getTilePairs(tiles) {
+  const pairs = {};
+
+  tiles.each((i, tile) => {
+    //notar que hay un espacio después de h-100
+    //amarillo
+    const colorClass = tile.className.replace(
+      "square h-full w-full transition-duration-500 ease-in-out ",
+      ""
+    );
+
+    if (pairs[colorClass]) {
+      pairs[colorClass].push(tile);
+    } else {
+      pairs[colorClass] = [tile];
+    }
+  });
+
+  // console.log(pairs);
+  return pairs;
+}
