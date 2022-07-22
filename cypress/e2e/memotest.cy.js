ba8b0cf5fa13ad2cd2a94c99d5529df56b0c3ec2
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-22 16:58:31 +0200

    creates test that checks for the board and the tiles

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -5,8 +5,9 @@ context("memotest", () => {
   before(() => {
     cy.visit(URL);
   });
+  it("checks if theres a board and tiles", () => {
+    cy.get("#board").find(".square").should("have.length", TILE_QTY);
+  });
 });
 
-it("checks if theres a board and tiles", () => {
-  cy.get("#board").find(".square").should("have.length", TILE_QTY);
-});
+//doesnt work
